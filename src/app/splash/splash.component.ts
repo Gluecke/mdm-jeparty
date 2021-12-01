8d11a3f05e662a60ef6e4a4673f872058a667b12
Garrett Luecke <14304172+Gluecke@users.noreply.github.com>
2021-11-30 20:47:32 -0600

    adds end of year holidays

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -7,21 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
   showSplash?: Boolean;
+  selectedImagePath?: string;
 
   constructor() { }
 
   ngOnInit(): void {
-    let endOfHalloween = new Date("2021-12-01T00:00:00");
+    let newYears = new Date("2022-01-01T00:00:00");
     let today = new Date();
 
-    if (today >= endOfHalloween) {
+    if (today >= newYears) {
       this.showSplash = false;
     } else {
       this.showSplash = true;
+
+      let randInt = this.getRandomFromRange(1, 5);
+
+      let selectedImage: string;
+
+      if (randInt === 1) {
+        selectedImage = "charlie-tree-xmas";
+      } else if (randInt === 2) {
+        selectedImage = "hanukkah-party";
+      } else if (randInt === 3) {
+        selectedImage = "kwanzaa-candle";
+      } else if (randInt === 4) {
+        selectedImage = "linus-tree-xmas";
+      } else {
+        selectedImage = "peanuts-happy-new-year";
+      }
+
+      this.selectedImagePath = `/assets/images/${selectedImage}.gif`;
     }
 
     setTimeout(() => {
       this.showSplash = false;
     }, 6800);
+  }
+
+  getRandomFromRange(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
