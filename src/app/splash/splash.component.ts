f628ac5787e365d6ea427382fcdad288ea468a29
Garrett <14304172+Gluecke@users.noreply.github.com>
2022-08-01 23:56:56 -0500

    adds video in place of splash gif

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -7,28 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
   showSplash?: Boolean;
+  showGif?: Boolean;
+  showVideo?: Boolean;
   selectedImagePath?: string;
+  selectedVideoPath?: string;
 
   constructor() { }
 
   ngOnInit(): void {
-    let expire = new Date("2022-08-01T00:00:00");
+    let expire = new Date("2022-09-01T00:00:00");
     let today = new Date();
+    this.showGif = false;
+    this.showVideo = true;
 
     if (today >= expire) {
       this.showSplash = false;
     } else {
       this.showSplash = true;
 
-      let startOfJune = new Date("2022-07-01T00:00:00");
-      if (today >= startOfJune) {
+      let startShowingSplash = new Date("2022-08-01T00:00:00");
+      if (today >= startShowingSplash) {
         this.selectedImagePath = `/assets/images/dancer.gif`;
+        this.selectedVideoPath = `/assets/video/ShortHit.mp4`;
       }
     }
 
     setTimeout(() => {
       this.showSplash = false;
-    }, 4000);
+    }, 7000);
   }
 
   getRandomFromRange(min: number, max: number): number {
