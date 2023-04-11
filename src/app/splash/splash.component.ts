45d29f0bbb1bbb5af2894acfb01bd30df8604150
Garrett <14304172+Gluecke@users.noreply.github.com>
2023-04-11 11:29:00 -0500

    gonna be may

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -16,7 +16,7 @@ export class SplashComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    let expire = new Date("2023-04-01T00:00:00");
+    let expire = new Date("2023-05-01T00:00:00");
     let today = new Date();
     this.showGif = true;
     this.showVideo = false;
@@ -28,7 +28,7 @@ export class SplashComponent implements OnInit {
 
       let startShowingSplash = new Date("2022-08-01T00:00:00");
       if (today >= startShowingSplash) {
-        let paths: string[] = ['weballin.gif'];
+        let paths: string[] = ['gonna-be-may.gif'];
         let selectedIndex = this.getRandomFromRange(0, 0);
         this.selectedImagePath = `/assets/images/${paths[selectedIndex]}`;
 
@@ -38,7 +38,7 @@ export class SplashComponent implements OnInit {
 
     setTimeout(() => {
       this.showSplash = false;
-    }, 7000);
+    }, 8000);
   }
 
   getRandomFromRange(min: number, max: number): number {
