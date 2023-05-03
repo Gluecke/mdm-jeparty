0cb345a0dec5742d5466641012f5eb03914558df
Garrett <14304172+Gluecke@users.noreply.github.com>
2023-05-02 22:01:42 -0500

    extends splash

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -16,7 +16,7 @@ export class SplashComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    let expire = new Date("2023-05-01T00:00:00");
+    let expire = new Date("2023-06-01T00:00:00");
     let today = new Date();
     this.showGif = true;
     this.showVideo = false;
