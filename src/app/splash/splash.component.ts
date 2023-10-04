f028df05866d36c1a7c88dee2a78927b18469a92
Garrett <14304172+Gluecke@users.noreply.github.com>
2023-10-04 13:53:40 -0500

    oct gifs

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -16,7 +16,7 @@ export class SplashComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    let expire = new Date("2023-10-01T00:00:00");
+    let expire = new Date("2023-11-01T00:00:00");
     let today = new Date();
     this.showGif = true;
     this.showVideo = false;
@@ -28,8 +28,8 @@ export class SplashComponent implements OnInit {
 
       let startShowingSplash = new Date("2022-08-01T00:00:00");
       if (today >= startShowingSplash) {
-        let paths: string[] = ['when-sept-ends.webp'];
-        let selectedIndex = this.getRandomFromRange(0, 0);
+        let paths: string[] = ['dem-bones.gif', 'air-five.gif', 'jack-o-bat.gif', 'knife-flip.gif'];
+        let selectedIndex = this.getRandomFromRange(0, 3);
         this.selectedImagePath = `/assets/images/${paths[selectedIndex]}`;
 
         // this.selectedVideoPath = `/assets/video/ShortHit.mp4`;
